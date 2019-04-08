b3d8dbf8d9c09b85ae0413d13284038f55bed166
Sewon Woo <sewon.dev@gmail.com>
2019-04-08 14:19:39 +0900

    Update request_crawling.py

--- Python/Crawling/request_crawling.py
+++ Python/Crawling/request_crawling.py
@@ -27,17 +27,16 @@ with requests.Session() as s:
 
 		# 입력받은 파일명들을 하나씩 접근하여 처리한다.
 		for file in files:
-			name = file.split('.')								# 파일명과 확장자를 분리한다.
-			search_url = 'https://boj.kr/' + name[0][3:]		# 검색 url을 만든다.
-			page = s.get(search_url)								# selenium으로 해당 문제를 연다.
-			html = page.text									# html을 받아온다.
-			soup = bs(html, 'html.parser')						# bs를 이용하여 파싱한다.
+			name = file.split('.')						# 파일명과 확장자를 분리한다.
+			search_url = 'https://boj.kr/' + name[0][3:]			# 검색 url을 만든다.
+			page = s.get(search_url)					# request로 해당 문제를 연다.
+			html = page.text						# html을 받아온다.
+			soup = bs(html, 'html.parser')					# bs를 이용하여 파싱한다.
 
-			problem_name = soup.select('#problem_title')[0].text 				# 문제 이름을 얻는다.
-			print(name[0][3:] + ' ' + name[1] + ' ' + problem_name, end=' ')	# 문제 번호, 확장자, 문제 이름을 출력
+			problem_name = soup.select('#problem_title')[0].text 		# 문제 이름을 얻는다.
+			print(name[0][3:] + ' ' + name[1] + ' ' + problem_name, end=' ')# 문제 번호, 확장자, 문제 이름을 출력
 
-			spoilers = soup.select('#problem_tags > ul > li')	# 알고리즘 분류에 해당되는 태그들을 가져온다.
-			for spoiler in spoilers:							# 발견된 태그들을 하나씩 출력한다.
+			spoilers = soup.select('#problem_tags > ul > li')		# 알고리즘 분류에 해당되는 태그들을 가져온다.
+			for spoiler in spoilers:					# 발견된 태그들을 하나씩 출력한다.
 				print(spoiler.text, end=' ')
 			print('')			
-		break	
