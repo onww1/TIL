3967ae321333a820b849c7686f6df60a4a169149
Sewon Woo <sewon.dev@gmail.com>
2019-04-08 12:57:36 +0900

    Update crawler.py

--- Python/Crawling/crawler.py
+++ Python/Crawling/crawler.py
@@ -33,19 +33,19 @@ while True:
 
 	# 입력받은 파일명들을 하나씩 접근하여 처리한다.
 	for file in files:
-		name = file.split('.')								# 파일명과 확장자를 분리한다.
-		search_url = 'https://boj.kr/' + name[0][3:]		# 검색 url을 만든다.
-		driver.get(search_url)								# selenium으로 해당 문제를 연다.
-		html = driver.page_source							# html을 받아온다.
+		name = file.split('.')							# 파일명과 확장자를 분리한다.
+		search_url = 'https://boj.kr/' + name[0][3:]				# 검색 url을 만든다.
+		driver.get(search_url)							# selenium으로 해당 문제를 연다.
+		html = driver.page_source						# html을 받아온다.
 		soup = bs(html, 'html.parser')						# bs를 이용하여 파싱한다.
 
-		problem_name = soup.select('#problem_title')[0].text 				# 문제 이름을 얻는다.
+		problem_name = soup.select('#problem_title')[0].text 			# 문제 이름을 얻는다.
 		print(name[0][3:] + ' ' + name[1] + ' ' + problem_name, end=' ')	# 문제 번호, 확장자, 문제 이름을 출력
 
-		spoilers = soup.select('#problem_tags > ul > li')	# 알고리즘 분류에 해당되는 태그들을 가져온다.
-		for spoiler in spoilers:							# 발견된 태그들을 하나씩 출력한다.
+		spoilers = soup.select('#problem_tags > ul > li')			# 알고리즘 분류에 해당되는 태그들을 가져온다.
+		for spoiler in spoilers:						# 발견된 태그들을 하나씩 출력한다.
 			print(spoiler.text, end=' ')
 		print('')			
-	break								# 마지막에 줄을 바꿔준다.
+	break										# 마지막에 줄을 바꿔준다.
 
 driver.quit()
